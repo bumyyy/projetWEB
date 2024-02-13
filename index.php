9ee14e000249d004100facf9d45c56fed57175d2
bumyyy <148772545+bumyyy@users.noreply.github.com>
2024-02-13 17:02:05 +0100

    verif mdp pb

--- index.php
+++ index.php
@@ -42,19 +42,20 @@
             
             <form action="verifierLogin.php" method="POST">
                 <div class="champemail">
-                    <input type="email" id="mail" name="Email" placeholder="Email" required>
+                    <input type="email" id="mail" name="mail" placeholder="Email" required>
                 </div>
                 <div class="champmdp">
-                    <input type="password" id="mdp" name="Mot de passe" placeholder="Mot de passe" required>
+                    <input type="password" id="mdp" name="mdp" placeholder="Mot de passe" required>
                 </div>
-            </form>
+           
 
-            <div class="check">
-                <input type="checkbox" id="showPassword" onclick="toggleVisibility()">
-                <label for="showPassword">Afficher le mot de passe</label>
-            </div>
-                
-            <button class="bouton">Identification</button>
+                <div class="check">
+                    <input type="checkbox" id="showPassword" onclick="toggleVisibility()">
+                    <label for="showPassword">Afficher le mot de passe</label>
+                </div>
+                    
+                <button type="submit" class="bouton">Identification</button>
+            </form>
             </div>
             
             <div id="errorMessage" style="color: red; display: none;">Mail ou mot de passe incorrect.</div>
--- verifierLogin.php
+++ verifierLogin.php
@@ -4,13 +4,18 @@ if (!empty($_POST['mail']) && !empty($_POST['mdp'])) {
     
     $mail = $_POST['mail'];
     $mdp = $_POST['mdp'];
-    $vrai_mdp = json_decode(file_get_contents("http://localhost/projetWEB/api/index.php?demande=authentification/" . urlencode($mail)));
-    
-    if ($vrai_mdp[0]->mdp == $mdp) {
+    $isSame = json_decode(file_get_contents("http://localhost/projetWEB/api/index.php?demande=authentification/" . urlencode($mail) . "/" . urlencode($mdp)));
+    print_r($mail);
+    if ($isSame) {
         // Le mail et le mot de passe correspondent
-        header(Location :"http://stagetier.fr/accueil"); // Redirigez vers la page souhaitée
+        //header("Location :http://stagetier.fr/accueil"); // Redirigez vers la page souhaitée
     } 
+    else {
+        // Le mail ou le mot de passe ne correspondent pas
+        //header("Location: http://stagetier.fr?error=invalid"); // Redirigez vers la page de login avec un paramètre d'erreur
+    }
 } else {
     // Les champs mail et mot de passe ne sont pas remplis
-    header("Location: votrePage.php?error=emptyfields");
+    //header("Location: http://stagetier.fr?error=emptyfields");
 }
+
