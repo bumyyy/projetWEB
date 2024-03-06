5a569a8f30b29b3587f9827f9f882d536f2fe86b
mathis2426 <mathiscucherat@gmail.com>
2024-03-06 14:09:16 +0100

    fix

---
+++ entreprise/Entreprises_stats.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stage_tier</title>
+    <link rel="stylesheet" href="Entreprises_stats.css">
+</head>
+<body>
+    
+<header class="header">
+    <div class="header-logo">
+        <img src="img/logo.png" alt="Logo">
+    </div>
+    <nav>
+        <ul class="header-nav">
+            <li>Stages</li>
+            <li>Entreprises</li>
+            <li>Pilotes</li>
+            <li>Étudiants</li>
+            <li>Candidature</li>
+        </ul>
+    </nav>
+    <div class="header-icons">
+        <img src="img/reglage.png" alt="Réglages">
+        <img src="img/profile.png" alt="Profil">
+    </div>
+</header>
+
+
+<!--La class carre représente ici les blocks dans lequels sont placé les informations lié aux entreprises-->
+
+<div class="main">
+<!--
+    <div class="ligne">
+    <div class="carre">
+            <div class="name">
+                <h1>Entreprise1</h1>
+                <p>informatique</p>
+            </div>
+        <div class="localité">
+            <h2>Localité</h2>
+            <p>Paris
+            Lyon lyon</p>
+        </div>
+        <div class="secteur">
+            <h2>Note</h2>
+            <div class="rating" data-rating="3">
+                <span class="star"></span>
+                <span class="star"></span>
+                <span class="star"></span>
+                <span class="star"></span>
+                <span class="star"></span>
+            </div>
+        </div> 
+        
+    </div>
+    </div>
+
+-->
+    
+<div class="Secteur" id="Secteur"></div>
+<div class="Localité" id="Localité"></div>
+
+
+    <div class="carre">
+            <div class="name">
+                <h1>Entreprise1</h1>
+                <div class="rating" data-rating="3">
+                <span class="star"></span>
+                <span class="star"></span>
+                <span class="star"></span>
+                <span class="star"></span>
+                <span class="star"></span>
+            </div>
+            </div>
+    <div class="downligne">
+        <div class="secteur">
+            <h2>Secteur</h2>
+            <p>secteur</p>
+            
+        </div> 
+        <div class="localité">
+            <h2>Localité</h2>
+            <p>Paris
+            Lyon lyon</p>
+        </div>
+        
+        </div> 
+
+
+
+</div>
+</div>
+<footer class="footer">
+    <img src="img/youtube.png" alt="YouTube">
+    <img src="img/linkedin.png" alt="LinkedIn">
+    <img src="img/facebook.png" alt="Facebook">
+    <img src="img/twitter.png" alt="Twitter">
+    <img src="img/instagram.png" alt="Instagram">
+    <p>© 2024 Stage_Tier - Tous droits réservés</p>
+</footer>
+<script src="Entreprises_stats.js"></script>
+</body>
+</html>
