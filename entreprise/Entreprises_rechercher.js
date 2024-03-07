a6f171d2399818698f079ce2c8a1dc09078e2579
mathis2426 <mathiscucherat@gmail.com>
2024-03-07 09:05:23 +0100

    js

--- entreprise/Entreprises_rechercher.js
+++ entreprise/Entreprises_rechercher.js
@@ -3,5 +3,18 @@ document.getElementById("create").addEventListener("click", function(){
 })
 
 document.getElementById("sub").addEventListener("change", function(){
-    ;
 })
+
+function highlightStars(value) {
+    const stars = document.querySelectorAll('.star');
+    alert(star);
+    stars.forEach(star => {
+      const starValue = parseInt(star.getAttribute('data-value'));
+      if (starValue <= value) {
+        star.style.color = '#ffc107'; // Change color to yellow
+      } else {
+        star.style.color = '#e4e5e9'; // Change color to gray
+      }
+    });
+  }
+highlightStars(2);
