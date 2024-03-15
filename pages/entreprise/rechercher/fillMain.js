e1d4f3d606ed39941f0ab73c743c634fea9d885b
mathis2426 <mathiscucherat@gmail.com>
2024-03-15 09:45:26 +0100

    fix popup

--- pages/entreprise/rechercher/fillMain.js
+++ pages/entreprise/rechercher/fillMain.js
@@ -49,9 +49,9 @@ document.addEventListener('DOMContentLoaded', function() {
             finalData.entreprises.forEach ((entreprise) => {  
                 tabnote.push(entreprise.moyenne_evaluations);
                 let html =
-                "<div class='completeEntreprise' onclick='toggleSubdivision(this)'>" +
+                "<div class='completeEntreprise'>" +
                 "<li class='ligne' id='"+entreprise.id_entreprise   +"'>" +
-                "    <div class='carre'>" +
+                "    <div class='carre' onclick='toggleSubdivision(this)'>" +
                 "        <div class='name'>" +
                 "            <h1 id='entrepriseName'>"+entreprise.nom_entreprise+"</h1>" +
                 "            <p>"+entreprise.secteur_activite+"</p>" +
@@ -76,6 +76,36 @@ document.addEventListener('DOMContentLoaded', function() {
                 "            <h2>Ont postulé</h2>"+
                 "            <p>"+entreprise.nb_stagiaires_postules+"</p>"+
                 "        </div>"+
+                "<div id='myModal' class='popdown'>"+
+                "<div class='fermer'><button id='closebtn'>x</button></div>"+
+                "<div class='name_popup'>"+
+                "<h1>"+entreprise.nom_entreprise+"</h1>"+
+                "<p>"+entreprise.secteur_activite+"</p>"+
+                "</div>"+
+                "<div class='localité_popup'>"+
+                "<h2>Localité</h2>"+
+                "<p>"+entreprise.ville+"</p>"+
+                "</div>"+
+                "<div class='secteur_popup'>"+
+                "<h2>Note</h2>"+
+                "<div class='rating' data-rating='3'>"+
+                    "<input type='hidden' id='rating-value' name='rating-value' value='0'>"+
+                    "<span class='star' data-value='5'>&#9733;</span>"+
+                    "<span class='star' data-value='4'>&#9733;</span>"+
+                    "<span class='star' data-value='3'>&#9733;</span>"+
+                    "<span class='star' data-value='2'>&#9733;</span>"+
+                    "<span class='star'' data-value='1'>&#9733;</span>"+
+                "</div>"+
+                "<div class='note' id='note'>0</div>"+
+                "</div>"+
+                "<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum urna ac metus varius, sit amet auctor nulla mollis. Integer eu elit velit.</p>"+
+                "<div class='lien_popup'>"+
+                "<a href='http://stagetier.fr/pages/stages'>rechercher entreprise dans stage</a>"+
+                "</div>"+
+                "</div>"+
+            
+
+
                 "    </div>";
                 if (userType != 3){
                 html +=
@@ -85,33 +115,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 "   </div>";
                 };
                 html +=
-                "<div id='myModal' class='popdown'>"+
-                    "<div class='fermer'><button id='closebtn'>x</button></div>"+
-                    "<div class='name_popup'>"+
-                    "<h1>"+entreprise.nom_entreprise+"</h1>"+
-                    "<p>"+entreprise.secteur_activite+"</p>"+
-                    "</div>"+
-                    "<div class='localité_popup'>"+
-                    "<h2>Localité</h2>"+
-                    "<p>"+entreprise.ville+"</p>"+
-                    "</div>"+
-                    "<div class='secteur_popup'>"+
-                    "<h2>Note</h2>"+
-                    "<div class='rating' data-rating='3'>"+
-                        "<input type='hidden' id='rating-value' name='rating-value' value='0'>"+
-                        "<span class='star' data-value='5'>&#9733;</span>"+
-                        "<span class='star' data-value='4'>&#9733;</span>"+
-                        "<span class='star' data-value='3'>&#9733;</span>"+
-                        "<span class='star' data-value='2'>&#9733;</span>"+
-                        "<span class='star'' data-value='1'>&#9733;</span>"+
-                    "</div>"+
-                    "<div class='note' id='note'>0</div>"+
-                    "</div>"+
-                    "<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum urna ac metus varius, sit amet auctor nulla mollis. Integer eu elit velit.</p>"+
-                    "<div class='lien_popup'>"+
-                    "<a href='http://stagetier.fr/pages/stages'>rechercher entreprise dans stage</a>"+
-                    "</div>"+
-                    "</div>"+
                 "</div>"+
                 "</div>";
                 
