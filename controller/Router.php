c28174939a81fd2f701a6b5762355dbce6cc7ed2
kkkipu <inakuzu01@gmail.com>
2024-03-16 01:44:19 +0100

    stagetier.fr me met à home.php, mais j'sais pas si c'est ce qu'il faut faire .-.

--- controller/Router.php
+++ controller/Router.php
@@ -7,24 +7,29 @@ class Router {
     private $request;
     private $routes = [
         "home.php" => ["controller" => "Home", "method" => "showHome"],
+        "" => ["controller" => "Home", "method" => "showHome"], // A voir si on enlève
     ];
 
-    public function __construct($request){
+    public function __construct($request) {
         $this->request = $request;
     }
 
-    public function renderController(){
+    public function renderController() {
         $request = $this->request;
 
-        if(isset($request, $this->routes)){
+        if(isset($this->routes[$request])){         // (isset($request, $this->routes))         c'était ça avant
             $controllerName = $this->routes[$request]['controller'];
             $methodName = $this->routes[$request]['method'];
             
             $currentController = new $controllerName();
             $currentController->$methodName(); // Utilisation de la variable contenant le nom de la méthode
         }
-        else{
+        else {
             echo '404';
         } 
     }
 }
+
+
+// http://stagetier.fr/?r=home.php      ça marche dans l'url, on arrive sur page d'accueil home.php
+// http://stagetier.fr                  j'essaie d'arriver sur la même page car j'sais pas s'il faut faire ça ._.
--- index.php
+++ index.php
@@ -5,7 +5,11 @@ Autoload::start();
 require_once CONTROLLER."Router.php";
 
 
-$request = $_GET['r'];
+// $request = $_GET['r'];
+$request = isset($_GET['r']) ? $_GET['r'] : 'home.php';
+// $request = isset($_GET['r']) ? $_GET['r'] : '';          // à voir si on enlève
+
+
 
 $routeur = new Router($request);
 $routeur->renderController();        
