5288926444be4ae2c2a7e293e0f3313646b06878
kkkipu <inakuzu01@gmail.com>
2024-03-22 15:04:31 +0100

    coeur sur candidature

--- controllers/FilterSearch.php
+++ controllers/FilterSearch.php
@@ -172,6 +172,41 @@ class FilterSearch extends Controller{
         // Encodage et envoi de la réponse
         echo json_encode($response);
     }
+
+
+    public function filterApplication($etat){
+
+        // Récupérer le contenu JSON de la requête
+        $jsonData = file_get_contents('php://input');
+    
+        // Décoder le contenu JSON en tableau associatif
+        $candidatures = json_decode($jsonData, true);
+        $candidatures = $candidatures['data'];
+    
+        // Filtrer les candidatures par etat
+        if ($etat != "x"){
+            $candidatures = array_filter($candidatures, function ($candidature) use ($etat) {
+                return $candidature['etat_candidature'] == $etat;
+            });
+        }
+
+        // Démarrer la session si elle n'est pas déjà démarrée
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    
+        // Récupérer le type d'utilisateur depuis la session
+        $utilisateur = isset($_SESSION['userData']['type']) ? $_SESSION['userData']['type'] : null;
+        
+        // Préparer la réponse
+        $response = [
+            'candidatures' =>    array_values($candidatures), // Réindexe et inclut les entreprises filtrées
+            'userType' => $utilisateur
+        ];
+        
+        // Encodage et envoi de la réponse
+        echo json_encode($response);
+    }
     
     
 }
--- public/application/fillMain.js
+++ public/application/fillMain.js
@@ -1,3 +1,4 @@
+    /*
 function toggleSubdivision(division) {
     let subdivision = division.querySelector('.popdown');
     let computedStyle = window.getComputedStyle(subdivision);
@@ -10,156 +11,168 @@ function toggleSubdivision(division) {
         document.body.classList.remove('no-scroll');
     }
 }
+*/
 
 document.addEventListener('DOMContentLoaded', function() {
 
-ROOT = 'http://stagetier.fr';
-
-document.getElementById('form').addEventListener('submit', function(e) {
-    e.preventDefault(); // Empêcher l'envoi traditionnel du formulaire
-    document.getElementById('main').innerHTML = ''; // reset la page
-
-    let dataSecteur = document.getElementById('secteur').value;
-    let dataVille = document.getElementById('ville').value;
-    let dataNote = document.getElementById('rate').value;
-    let dataSearch = document.getElementById('search').value;
+    ROOT = 'http://stagetier.fr';
     
-    let URL_ = dataSearch !== "" 
-        ? `${ROOT}/ApiManager/company/companyBySearch/${dataSearch}` 
-        : `${ROOT}/ApiManager/company/allCompany/`;
-    fetch(URL_)
-    .then(response => response.json())
-    .then(dataResponse => {
-        return fetch(`${ROOT}/FilterSearch/filter/${encodeURIComponent(dataSecteur)}/${encodeURIComponent(dataVille)}/${encodeURIComponent(dataNote)}`, {
-            method: 'POST', 
-            headers: {
-                'Content-Type': 'application/json', 
-            },
-            body: JSON.stringify({
-                data: dataResponse, 
-            })
-        });
-    })
-    .then(finalData => finalData.json()) 
-    .then(finalData => {
-        let userType = finalData.userType;
-        let tabnote = [];
-
-        finalData.entreprises.forEach ((entreprise) => { 
-            tabnote.push(entreprise.moyenne_evaluations);
-            let html =
-            "<div class='completeEntreprise'>" +
-            "<li class='ligne' id='"+entreprise.id_entreprise   +"'>" +
-            "    <div class='carre' onclick='toggleSubdivision(this)'>" +
-            "        <div class='name'>" +
-            "            <h1 id='entrepriseName'>"+entreprise.nom_entreprise+"</h1>" +
-            "            <p>"+entreprise.secteur_activite+"</p>" +
-            "        </div>" +
-            "        <div class='localité'>" +
-            "            <h2>Localité</h2>" +
-            "            <p>"+entreprise.ville+"</p>" +
-            "        </div>" +
-            "        <div class='secteur'>" +
-            "            <h2>Note</h2>" +
-            "            <div class='rating'>"+
-            "            <input type='hidden' id='rating-value' name='rating-value' value='0'>"+
-            "            <span class='star' data-value='5'>&#9733;</span>" +
-            "            <span class='star' data-value='4'>&#9733;</span>" +
-            "            <span class='star' data-value='3'>&#9733;</span>" +
-            "            <span class='star' data-value='2'>&#9733;</span>" +
-            "            <span class='star' data-value='1'>&#9733;</span>" +
-            "        </div>"+
-            "        <div class='note' id='note'>0</div>"+
-            "        </div>"+            
-            "        <div class='localité'>"+
-            "            <h2>Ont postulé</h2>"+
-            "            <p>"+entreprise.nb_stagiaires_postules+"</p>"+
-            "        </div>"+
-            
-            "<div id='myModal' class='popdown'>"+
-            "<div class='fermer'><button id='closebtn'>x</button></div>"+
-            "<div class='name_popup'>"+
-            "<h1>"+entreprise.nom_entreprise+"</h1>"+
-            "<p>"+entreprise.secteur_activite+"</p>"+
-            "</div>"+
-            "<div class='localité_popup'>"+
-            "<h2>Localité</h2>"+
-            "<p>"+entreprise.ville+"</p>"+
-            "</div>"+
-            "<div class='secteur_popup'>"+
-            "<h2>Note</h2>"+
-            "<div class='rating' data-rating='3'>"+
-                "<input type='hidden' id='rating-value' name='rating-value' value='0'>"+
-                "<span class='star' data-value='5'>&#9733;</span>"+
-                "<span class='star' data-value='4'>&#9733;</span>"+
-                "<span class='star' data-value='3'>&#9733;</span>"+
-                "<span class='star' data-value='2'>&#9733;</span>"+
-                "<span class='star'' data-value='1'>&#9733;</span>"+
-            "</div>"+
-            "<div class='note' id='note'>0</div>"+
-            "</div>"+
-            "<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum urna ac metus varius, sit amet auctor nulla mollis. Integer eu elit velit.</p>"+
-            "<div class='lien_popup'>"+
-            "<a href='http://stagetier.fr/pages/stages'>rechercher entreprise dans stage</a>"+
-            "</div>"+
-            "</div>"+
+    document.getElementById('form').addEventListener('submit', function(e) {
+        e.preventDefault(); // Empêcher l'envoi traditionnel du formulaire
+        document.getElementById('main').innerHTML = ''; // reset la page
+    
         
-
-
-            "    </div>";
-            if (userType != 3){
-            html +=
-            "   <div class='mod'>"+
-            "       <span onclick=update("+entreprise.id_entreprise+") class='update'></span>"+
-            "       <span onclick=confirmerSuppression("+entreprise.id_entreprise+") class='delete'></span>"+
-            "   </div>";
-            };
-            html +=
-            "</div>"+
-            "</div>";
-            
-            document.getElementById('main').innerHTML += html;
+        let dataEtat = document.getElementById('etat').value;
+        let dataSearch = document.getElementById('search').value;
+        
+        let URL_ = dataSearch !== "" 
+            ? `${ROOT}/ApiManager/application/applicationBySearch/${dataSearch}` 
+            : `${ROOT}/ApiManager/application/allApplication/`;
+        fetch(URL_)
+        .then(response => response.json())
+        .then(dataResponse => {
+            return fetch(`${ROOT}/FilterSearch/filterApplication/${dataEtat}`, {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json', 
+                },
+                
+                body: JSON.stringify({
+                    data: dataResponse, 
+                })
+            });
         })
-        highlightStars(tabnote);
-        pagination();
-    })
-    .catch(error => console.error('Error:', error));
-});
+        .then(finalData => finalData.json()) 
+        .then(finalData => {
+            let userType = finalData.userType;
+            let tabnote = [];
+            finalData.candidatures.forEach ((candidature) => {
+                tabnote.push(candidature.etat_candidature);
+                let html =
+                "<div class='completeEntreprise'>" +
+                "<li class='ligne' id='"+candidature.id_offre      +"'>" +
+                "    <div class='carre' onclick='toggleSubdivision(this)'>" +
+                "        <div class='name'>" +
+                "            <h1 id='entrepriseName'>"+candidature.nom_offre+"</h1>" +
+                "            <p>"+candidature.nom_entreprise+"</p>" +
+                "        </div>" +
+                "        <div class='localité'>" +
+                "            <h2>"+candidature.ville+"</h2>" +
+                "            <p>"+candidature.competences_requises+"</p>" +
+                "        </div>" +
+                "        <div class='secteur'>" +
+                "            <h2>"+candidature.nom_promotion+"</h2>" +
+                "            <p>"+candidature.date_debut_offre+"</p>" +
+                "            <p>"+candidature.date_fin_offre+"</p>" +
+                "        </div>"+
+                "        <div class='localité'>"+
+                "            <h2>"+candidature.remuneration_base+"</h2>"+
+                "            <p>"+candidature.duree_mois_stage+"</p>"+
+                "        </div>"+ 
+                "        <div class='localité'>"+
+                "            <h2>"+candidature.nombre_places_offertes+"</h2>"+
+                "            <p>"+candidature.nombre_etudiants_postules+"</p>"+
+                "        </div>"+        
+       /*         "<div id='myModal' class='popdown'>"+
+                "<div class='fermer'><button id='closebtn'>x</button></div>"+
+                "<div class='name_popup'>"+
+                "<h1>"+stage.nom_entreprise+"</h1>"+
+                "<p>"+stage.secteur_activite+"</p>"+
+                "</div>"+
+                "<div class='localité_popup'>"+
+                "<h2>Localité</h2>"+
+                "<p>"+stage.ville+"</p>"+
+                "</div>"+
+                "<div class='secteur_popup'>"+
+                "<h2>Note</h2>"+
+                "<div class='rating' data-rating='3'>"+
+                    "<input type='hidden' id='rating-value' name='rating-value' value='0'>"+
+                    "<span class='star' data-value='5'>&#9733;</span>"+
+                    "<span class='star' data-value='4'>&#9733;</span>"+
+                    "<span class='star' data-value='3'>&#9733;</span>"+
+                    "<span class='star' data-value='2'>&#9733;</span>"+
+                    "<span class='star'' data-value='1'>&#9733;</span>"+
+                "</div>"+
+                "<div class='note' id='note'>0</div>"+
+                "</div>"+
+                "<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum urna ac metus varius, sit amet auctor nulla mollis. Integer eu elit velit.</p>"+
+                "<div class='lien_popup'>"+
+                "<a href='http://stagetier.fr/pages/stages'>rechercher entreprise dans stage</a>"+
+                "</div>"+
+                "</div>"+
 
 
+                */
+                "    </div>";
+                
+                if (userType != 3){
+                html +=
+                "   <div class='mod'>"+
+                "       <span onclick=favorite("+candidature.id_offre+") class='heart' data-value="+candidature.etat_candidature+">&#9829;</span>"+
+                        "<input type='hidden' id='rating-value' name='rating-value' value='0'>"+
+                "       <span onclick=update("+candidature.id_offre+") class='update'></span>"+
+                "       <span onclick=confirmerSuppression("+candidature.id_offre+") class='delete'></span>"+
+                "   </div>";
+                };
+                html +=
+                "</div>"+
+                "</div>";
+                
+                document.getElementById('main').innerHTML += html;
+                
+            })
+
+            // Gérer le cœur de notation
+            let isHeartSelected = false; // Variable pour vérifier si une note a été sélectionnée
+            const hearts = document.querySelectorAll('.heart');
+            const heartValue = document.getElementById('rating-value');
+
+            hearts.forEach(heart => {
+                heart.addEventListener('click', () => {
+                    const id_offre = heart.parentElement.parentElement.parentElement.id;
+                    const value = parseInt(heart.getAttribute('data-value'));
+                    
+                    // Mettre à jour la valeur de data-value à 1
+                    heart.setAttribute('data-value', '1');
+                    
+                    // Mettre à jour la couleur du cœur
+                    highlightHeart(1); // Mettre à jour la couleur en fonction de la nouvelle valeur
+                    
+                    // Appeler la fonction favorite avec l'ID de l'offre
+                    favorite(id_offre);
+                    
+                    // Mettre à jour la variable pour indiquer qu'une note a été sélectionnée
+                    isHeartSelected = true;
+                    
+                    // Vous pouvez maintenant envoyer la valeur sélectionnée au serveur ou effectuer d'autres actions nécessaires
+                });
+            });
 
 
+            function highlightHeart(value) {
+                hearts.forEach(heart => {
+                    const heartValue = parseInt(heart.getAttribute('data-value'));
+                    if (heartValue <= value) {
+                        heart.style.color = '#fe8bb2'; // Changer la couleur en jaune
+                    } else {
+                        heart.style.color = '#e4e5e9'; // Changer la couleur en gris
+                    }
+                });
+            }
+            
+            highlightHeart(tabnote);
+            console.log(tabnote);
+            //highlightStars(tabnote);
+            pagination();
+        })
+        .catch(error => console.error('Error:', error));
+    });
+    
+    
+    });//fin du domloadcontent
 
-//coloris les etoiles en fonction du nom des entreprises et du tableau de note
-function highlightStars(tabnote) {
-const stars = document.querySelectorAll('.star');
-
-for (let a = 1; a <= stars.length / 10; a++) {
-    let idebut = (a - 1) * 10;
-    let ifin = (a * 10);
-    if (ifin > stars.length) {
-        ifin = stars.length;
+    function favorite(id_offre) {
+        // Ici, vous pouvez ajouter la logique pour marquer l'offre avec l'ID spécifié comme favori
+        console.log("Offre avec l'ID " + id_offre + " ajoutée aux favoris.");
     }
-  for(let i= idebut;i<ifin;i++){
-      const starValue = parseInt(stars[i].getAttribute('data-value'));
-      if (starValue <= parseInt(tabnote[a-1])) {
-        stars[i].style.color = '#ffc107'; // Change color to yellow
-      } else {
-        stars[i].style.color = 'rgb(179,179,179)'; // Change color to gray
-      }
-    }
-  }
 
-  let elements = document.querySelectorAll('[id="note"]');
-
-for(let i=0;i<elements.length/2;i++){
-  let c = i*2;
-  elements[c].textContent=parseFloat(tabnote[i]).toFixed(1);
-  elements[c+1].textContent=parseFloat(tabnote[i]).toFixed(1);
-}
-
-}
-
-
-
-});//fin du domloadcontent
