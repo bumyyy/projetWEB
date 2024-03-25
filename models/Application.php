29ac06c2c5be144757daaba427d36c3c432ca366
kkkipu <inakuzu01@gmail.com>
2024-03-25 23:28:04 +0100

    filtre favori marche, ajouter + supprimer favori marche dans la bdd, mais pas dans le front

--- models/Application.php
+++ models/Application.php
@@ -13,19 +13,23 @@ class Application extends Model {
         promotion.nom AS nom_promotion,
         stage.date_debut AS date_debut_offre, 
         stage.date_fin AS date_fin_offre,
-        DATEDIFF(stage.date_fin, stage.date_debut) AS duree_stage, -- Calcul de la durée du stage en jour
-        FLOOR(DATEDIFF(stage.date_fin, stage.date_debut) / 30) AS duree_mois_stage, -- Durée du stage en mois (approximatif)
+        DATEDIFF(stage.date_fin, stage.date_debut) AS duree_stage,
+        FLOOR(DATEDIFF(stage.date_fin, stage.date_debut) / 30) AS duree_mois_stage,
         stage.remuneration AS remuneration_base,
         stage.nb_place AS nombre_places_offertes,
         COUNT(DISTINCT candidater.id_utilisateur) AS nombre_etudiants_postules,
-        candidater.etat AS etat_candidature
+        candidater.etat AS etat_candidature,
+        CASE WHEN aimer.id_utilisateur IS NOT NULL THEN aimer.id_stage ELSE NULL END AS id_stage_aimé,
+        CASE WHEN candidater.id_utilisateur IS NOT NULL THEN stage.id ELSE NULL END AS id_stage_candidaté
         FROM stage
         INNER JOIN entreprise ON stage.id_entreprise = entreprise.id
         LEFT JOIN rechercher ON stage.id = rechercher.id_stage
         LEFT JOIN competence ON competence.id = rechercher.id_competence
         LEFT JOIN ville ON stage.id_ville = ville.id
         LEFT JOIN promotion ON stage.id_promotion = promotion.id
-        LEFT JOIN candidater ON stage.id = candidater.id_stage
+        LEFT JOIN candidater ON stage.id = candidater.id_stage AND candidater.id_utilisateur = 1
+        LEFT JOIN aimer ON stage.id = aimer.id_stage AND aimer.id_utilisateur = 1
+        WHERE aimer.id_utilisateur = 1 OR candidater.id_utilisateur = 1
         GROUP BY stage.id";
         $stmt = $this->conn->prepare($sql); 
         $stmt->execute(); 
@@ -44,20 +48,24 @@ class Application extends Model {
         promotion.nom AS nom_promotion,
         stage.date_debut AS date_debut_offre, 
         stage.date_fin AS date_fin_offre,
-        DATEDIFF(stage.date_fin, stage.date_debut) AS duree_stage, -- Calcul de la durée du stage en jour
-        FLOOR(DATEDIFF(stage.date_fin, stage.date_debut) / 30) AS duree_mois_stage, -- Durée du stage en mois (approximatif)
+        DATEDIFF(stage.date_fin, stage.date_debut) AS duree_stage,
+        FLOOR(DATEDIFF(stage.date_fin, stage.date_debut) / 30) AS duree_mois_stage,
         stage.remuneration AS remuneration_base,
         stage.nb_place AS nombre_places_offertes,
         COUNT(DISTINCT candidater.id_utilisateur) AS nombre_etudiants_postules,
-        candidater.etat AS etat_candidature
+        candidater.etat AS etat_candidature,
+        CASE WHEN aimer.id_utilisateur IS NOT NULL THEN aimer.id_stage ELSE NULL END AS id_stage_aimé,
+        CASE WHEN candidater.id_utilisateur IS NOT NULL THEN stage.id ELSE NULL END AS id_stage_candidaté
         FROM stage
         INNER JOIN entreprise ON stage.id_entreprise = entreprise.id
         LEFT JOIN rechercher ON stage.id = rechercher.id_stage
         LEFT JOIN competence ON competence.id = rechercher.id_competence
         LEFT JOIN ville ON stage.id_ville = ville.id
         LEFT JOIN promotion ON stage.id_promotion = promotion.id
-        LEFT JOIN candidater ON stage.id = candidater.id_stage
-        WHERE stage.nom LIKE :recherche
+        LEFT JOIN candidater ON stage.id = candidater.id_stage AND candidater.id_utilisateur = 1
+        LEFT JOIN aimer ON stage.id = aimer.id_stage AND aimer.id_utilisateur = 1
+        WHERE stage_nom LIKE :recherche
+        AND aimer.id_utilisateur = 1 OR candidater.id_utilisateur = 1
         GROUP BY stage.id";
         $stmt = $this->conn->prepare($sql); 
         $stmt->execute(['recherche' => '%'.$search.'%']); //permet de bind values et d'ajouter les % pour le like
@@ -81,7 +89,8 @@ class Application extends Model {
         stage.remuneration AS remuneration_base,
         stage.nb_place AS nombre_places_offertes,
         COUNT(DISTINCT candidater.id_utilisateur) AS nombre_etudiants_postules,
-        candidater.etat AS etat_candidature
+        candidater.etat AS etat_candidature,
+        aimer.id_stage AS id_stage
         FROM stage
         INNER JOIN entreprise ON stage.id_entreprise = entreprise.id
         LEFT JOIN rechercher ON stage.id = rechercher.id_stage
